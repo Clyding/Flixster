0322010063c69b495fd5110a44312d15e477a5c6
Clyde Baidoo <105178839+Clyding@users.noreply.github.com>
2024-12-01 22:10:46 -0500

    Add files via upload

---
+++ app/build/src/main/java/com/example/flixster/Glide.kt
@@ -0,0 +1,9 @@
+package com.example.flixster
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class Glide : AppGlideModule() {
+
+}
